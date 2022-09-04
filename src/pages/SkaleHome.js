0a886468cc4f7ff0c883e3e72118690b73c266a7
Djeko17 <djekic1313@gmail.com>
2022-09-04 19:32:34 +0200

    Napravljena funkc. komponenta SkaleHome, ubacen Bootstrap

---
+++ src/pages/SkaleHome.js
@@ -0,0 +1,20 @@
+const SkaleHome = () => {
+
+    const [ucesnici, setUcesnici] = useState([
+        { id: 1, ime: "Nevena", prezime: "Božović", pesma: "Kraj" },
+        { id: 2, ime: "Zorja", prezime: "", pesma: "Lavine" },
+        { id: 3, ime: "Rasta x Link", prezime: "", pesma: "Dan i noć" },
+        { id: 4, ime: "Zoi", prezime: "", pesma: "Problem" },
+        { id: 5, ime: "Albino", prezime: "", pesma: "Carstvo poroka" },
+        { id: 6, ime: "Grupa Klon", prezime: "", pesma: "Nisam ti ja taj" },
+
+    ]);
+
+    return (
+        <div className="skale">
+
+        </div>
+    )
+}
+
+export default SkaleHome;
