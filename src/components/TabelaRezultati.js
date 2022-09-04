fe5cd5208642b282cef65d993948cd04a95f83db
Djeko17 <djekic1313@gmail.com>
2022-09-04 20:59:14 +0200

    Postavljena tabela sa rezultatima glasanja u komponenti TabelaRezultati

--- src/components/TabelaRezultati.js
+++ src/components/TabelaRezultati.js
@@ -1,11 +1,36 @@
 const TabelaRezultati = (props) => {
 
     var ucesnici = props.ucesnici;
+    const [ucesniciTabela, setUcesniciTabela] = useState([ucesnici]);
+
 
     return (
         <div className="tabelarezultati">
 
-        </div>
+            <table className="table" id="tabela-glasanje">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Ime i prezime</th>
+                        <th>Pesma</th>
+                        <th>Broj glasova</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ucesnici.map(ucesnik => {
+                        return (
+                            <tr key={ucesnik.id}>
+                                <td>{ucesnik.id}</td>
+                                <td>{ucesnik.ime} {" " + ucesnik.prezime}</td>
+                                <td>{ucesnik.pesma}</td>
+                                <td>{ucesnik.brojGlasova}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+
+        </div >
     )
 }
 
